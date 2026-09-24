4010b83e84041a5373a4883ebc15a562c721323a
808StaN <d.stanisz00@gmail.com>
2026-09-24 23:00:48 +0200

    perf: optimize shader imports

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   poweredByHeader: false,
   serverExternalPackages: ["pdf-parse"],
+  experimental: {
+    optimizePackageImports: ["shaders/react"],
+  },
 };
 
 export default nextConfig;
